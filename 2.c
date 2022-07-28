aec50d442ef0d2faec23c693fd663311ad3e6160
9TSe <110165870+9TSe@users.noreply.github.com>
2022-07-28 19:24:35 +0800

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int line = 0;
+	while (line < 30000)
+	{
+		printf("找offer:%d\n", line);
+		line++;
+	}
+	if (line = 30000)
+	{
+		printf("收到了一个贼牛逼的offer");
+	}
+	return  0;
+}
