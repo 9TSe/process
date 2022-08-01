7c1b9549c47e73582133977a4ad9b587870a0171
9TSe <110165870+9TSe@users.noreply.github.com>
2022-08-01 11:38:02 +0800

    Add files via upload

---
+++ 13.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+struct baishuyuanps
+{
+	int age;
+	char name[20];
+	float weight;
+	int score;
+};
+int main()
+{
+	struct baishuyuanps s = { 18,"°×ÊéÔ´",102,1433223 };
+	printf("1:%d\n%s\n%lf\n%d\n", s.age, s.name, s.weight, s.score);
+	struct  baishuyuanps * pc = &s;
+	printf("2:%d\n%s\n%lf\n%d\n", (*pc).age, (*pc).name, (*pc).weight, (*pc).score);
+
+	printf("3:%d\n%s\n%lf\n%d\n", pc->age, pc->name, pc->weight, pc->score);
+
+	return 0;
+}
