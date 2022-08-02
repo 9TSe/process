98ec3d535e71c620825c2028940b02340adbcc39
9TSe <110165870+9TSe@users.noreply.github.com>
2022-08-02 15:52:45 +0800

    Update 18.c

--- 18.c
+++ 18.c
@@ -18,9 +18,9 @@ int main()
 		case 2:
 			m++;
 			n++;
-			
+			//这里加不加break效果都一样,直接从该局部switch跳出,不全部终止
 		}
-	case 999:
+	case 999:   //直接跳入这里,并不是判定第一个  switch(n)  之后选择进入该case
 		m++;
 		break;
 	default:
