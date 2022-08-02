b2e41d37720a14c7c7d8c3b79daffe310a19b79e
9TSe <110165870+9TSe@users.noreply.github.com>
2022-08-02 15:12:33 +0800

    Add files via upload

---
+++ 17.c
@@ -0,0 +1,15 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	scanf("%d", &i);
+	switch (i)
+	{
+	case 1:
+			printf("111");
+	case 2:
+		    printf("222");
+	}
+	return 0;
+}
