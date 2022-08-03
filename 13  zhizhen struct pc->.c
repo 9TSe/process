d8a0db1dba7bb2c9589eaf09a4e1d4b921c2aa17
9TSe <110165870+9TSe@users.noreply.github.com>
2022-08-03 17:50:33 +0800

    Update and rename 13.c to 13  zhizhen struct pc->.c

--- 13  zhizhen struct pc->.c
+++ 13  zhizhen struct pc->.c
@@ -8,7 +8,7 @@ struct baishuyuanps
 };
 int main()
 {
-	struct baishuyuanps s = { 18,"°×ÊéÔ´",102,1433223 };
+	struct baishuyuanps s = { 18,"baishuyuan",102,1433223 };
 	printf("1:%d\n%s\n%lf\n%d\n", s.age, s.name, s.weight, s.score);
 	struct  baishuyuanps * pc = &s;
 	printf("2:%d\n%s\n%lf\n%d\n", (*pc).age, (*pc).name, (*pc).weight, (*pc).score);
