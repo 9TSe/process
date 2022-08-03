6ed63443b172acd389069cd2db5b0db2206a6f29
9TSe <110165870+9TSe@users.noreply.github.com>
2022-08-03 17:40:51 +0800

    Rename 9.c to 9typedef.c

--- 9.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-typedef unsigned int u_int; //使得 typedef unsigned 可以用 u_int 来表示
-int main()
-{
-	unsigned int num1 = 100;   //于是这一行与下一行表达意思完全一样
-	u_int num1 = 100;
-	printf("%d", num1); //该项代码只做演示 不求运行
-	return 0;
-}
