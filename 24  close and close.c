98ebdb93ed67dd07f10b075cadfcd0df54755d4a
9TSe <110165870+9TSe@users.noreply.github.com>
2022-08-11 19:55:23 +0800

    Update 24  close and close.c

--- 24  close and close.c
+++ 24  close and close.c
@@ -1,14 +1,18 @@
-#define  _CRT_SECURE_NO_WARNINGS
+#include<windows.h>  //Sleep 的头文件
 #include<stdio.h>
 int main()
 {
 	char arr1[] = "hello world";
 	char arr2[] = "#$%#%^&*^&*";
+	
 	int left = 0;
 	int right = strlen(arr1) -1;
+	
 	while (left <= right)
 	{  
 		printf("%s\n", arr2);
+		Sleep(1000);   //以毫秒为单位 S为大写
+		
 		arr2[left] = arr1[left];
 		arr2[right] = arr1[right];
 		
@@ -16,7 +20,5 @@ int main()
 		right --;
 
 	}
-	
-
 	return 0;
 }
