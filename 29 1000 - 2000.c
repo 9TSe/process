10f5c1b33b6b07dfc4d9d813904eaf6c7afb75c0
9TSe <110165870+9TSe@users.noreply.github.com>
2022-08-29 16:11:09 +0800

    Add files via upload

---
+++ 29 1000 - 2000.c
@@ -0,0 +1,17 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int year = 0;
+	int count = 0;
+	for (year = 1000; year <= 2000; year++)
+	{
+		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))// ||表示或者   &&表示并且
+		{
+			printf("%d ", year);
+			count++;
+		}
+	}
+	printf("\n%d\n", count);
+	return 0;
+}
