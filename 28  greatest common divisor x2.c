5bd0d9841dc4321caef86a70200dec0f0b228f8e
9TSe <110165870+9TSe@users.noreply.github.com>
2022-08-29 15:11:40 +0800

    Update and rename 28  greatest common divisor.c to 28  greatest common divisor x2.c

--- 28  greatest common divisor x2.c
+++ 28  greatest common divisor x2.c
@@ -6,13 +6,13 @@ int main()
 	int a = 0;
 	int b = 0;
 	scanf("%d%d", &a, &b);
-	int max = 0;
-	if (a > b)
-	{
-		max = b;
-	}
-	else
-	{
+	int max = 0;        // int c = 0 ;   辗转相除法
+	if (a > b)          // while( c = a % b)
+	{                   //{
+		max = b;    //  b = a;
+	}                   //  c = b;
+	else                // }
+	{                   // printf("the greatest common divisor is %d",b)
 		max = a;
 	}
 	while (1)
