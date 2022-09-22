596d3e6d3942bb06498e7927333792ed9761ed17
9TSe <110165870+9TSe@users.noreply.github.com>
2022-09-22 09:34:37 +0800

    Update 34 strcpy.c

--- 34 strcpy.c
+++ 34 strcpy.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-#include<string.h>
+#include<string.h>   //引用 strcpy 的头文件 
 int main()
 {
     char arr1[20] = { 0 };
     char arr2[] = "hello bit";
-    strcpy(arr1, arr2);
+    strcpy(arr1, arr2);    
     printf("%s", arr1);
     return 0;
 }
