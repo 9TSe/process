113268fe9efc1a82a1f66b9bf44f2c726f9b0c57
9TSe <110165870+9TSe@users.noreply.github.com>
2022-12-09 19:56:48 +0800

    Update and rename 13  zhizhen struct pc->.c to 4 结构体的初见.c

--- 4 结构体的初见.c
+++ 4 结构体的初见.c
@@ -10,7 +10,7 @@ int main()
 {
 	struct baishuyuanps s = { 18,"baishuyuan",102,1433223 };
 	printf("1:%d\n%s\n%lf\n%d\n", s.age, s.name, s.weight, s.score);
-	struct  baishuyuanps * pc = &s;
+	struct baishuyuanps * pc = &s;
 	printf("2:%d\n%s\n%lf\n%d\n", (*pc).age, (*pc).name, (*pc).weight, (*pc).score);
 
 	printf("3:%d\n%s\n%lf\n%d\n", pc->age, pc->name, pc->weight, pc->score);
