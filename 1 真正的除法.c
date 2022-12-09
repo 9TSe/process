b51e17b1018783e207bcd008847bd8df1e92f63d
9TSe <110165870+9TSe@users.noreply.github.com>
2022-12-09 19:37:57 +0800

    Update and rename 6  without %.c to 1 真正的除法.c

---
+++ 1 真正的除法.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	
+	double a = 9 / 2;   //这种方法无法得到真正的除法
+	double b = 9 / 2.0;
+	double c = (double)9 / 2;
+	printf("%lf\n", a);
+	printf("%lf\n", b);
+	printf("%lf\n", c);
+	return 0;
+}
